cb469bd6b467367aa40d15f2db0fe8df49ec6a96
Lucas <lucas@agtlucas.com>
2014-11-10 23:44:21 -0200

    Update app.js

--- src/js/TodoApp.js
+++ src/js/TodoApp.js
@@ -8,4 +8,23 @@ var TodoApp = React.createClass({
   getInitialState: function () {
     return {items: [], text: ""};
   },
+
+  componentWillMount: function () {
+    var firebaseRef = new Firebase('https://ReactFireTodoApp.firebaseio.com/items/');
+    this.bindAsArray(firebaseRef.limitToLast(25), 'items');
+  },
+
+  onChange: function (e) {
+    this.setState({text: e.target.value});
+  },
+
+  handleSubmit: function (e) {
+    e.preventDefault();
+    if (this.state.text && this.state.text.trim() !== 0) {
+      this.firebaseRef['items'].push({
+        text: this.state.text
+      });
+      this.setState({text: ""});
+    }
+  }
 })
